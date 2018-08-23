c6ad446332e4903f64b1367d944f317113305a57
Jorge <jorge.costa@developer.pt>
2018-08-24 00:30:52 +0100

    Don't show stacked icon on parent blocks if all of its children are hidden from the inserter.

--- packages/editor/src/store/test/selectors.js
+++ packages/editor/src/store/test/selectors.js
@@ -3030,7 +3030,7 @@ describe( 'selectors', () => {
 				isDisabled: false,
 				utility: 0,
 				frecency: 0,
-				hasChildBlocks: false,
+				hasChildBlocksWithInserterSupport: false,
 			} );
 			const reusableBlockItem = items.find( ( item ) => item.id === 'core/block/1' );
 			expect( reusableBlockItem ).toEqual( {
